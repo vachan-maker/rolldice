e4644a2159cf128a967f9c19cac4f8199b86e812
vachan-maker <vachan@tutanota.com>
2026-06-25 12:31:09 +0530

    Addd gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World'))),
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 207, 53, 19),
+                Color.fromARGB(255, 255, 92, 92),
+              ],
+            ),
+          ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
+          child: const Center(child: Text('Hello World')),
+        ),
+      ),
     ),
   );
 }
