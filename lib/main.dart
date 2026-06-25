f0a532b4fb13f80348c95b64134eec5e54465ed1
vachan-maker <vachan@tutanota.com>
2026-06-25 12:48:29 +0530

    Created a new widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 207, 53, 19),
-                Color.fromARGB(255, 255, 92, 92),
-              ],
-            ),
-          ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
-          child: const Center(child: Text('Hello World')),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 207, 53, 19),
+            Color.fromARGB(255, 255, 92, 92),
+          ],
+        ),
+      ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(color: Colors.white, fontSize: 28),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
