dceef511eb759e43512b93c8cee7a500194d2420
vachan-maker <vachan@tutanota.com>
2026-06-25 13:15:00 +0530

    Use variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:rolldice/text_container.dart';
 
+const startGradient = Color.fromARGB(255, 207, 53, 19);
+const stopGradient = Color.fromARGB(255, 255, 92, 92);
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -8,12 +11,7 @@ class GradientContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 207, 53, 19),
-            Color.fromARGB(255, 255, 92, 92),
-          ],
-        ),
+        gradient: LinearGradient(colors: [startGradient, stopGradient]),
       ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
       child: Center(child: TextContainer()),
     );
