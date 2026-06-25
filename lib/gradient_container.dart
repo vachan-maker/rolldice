7c6ffcc07c898d75da578bfd46bc007e6e4f4402
vachan-maker <vachan@tutanota.com>
2026-06-25 14:07:10 +0530

    make gradient color widget dynamic in nature

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,13 +5,15 @@ const Color startGradient = Color.fromARGB(255, 207, 53, 19);
 const Color stopGradient = Color.fromARGB(255, 255, 92, 92);
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.startColor, this.stopColor, {super.key});
+  final Color startColor;
+  final Color stopColor;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [startGradient, stopGradient]),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [startColor, stopColor]),
       ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
       child: Center(child: TextContainer('My Name is Vachan')),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'gradient_container.dart';
 
+const Color startGradient = Color.fromARGB(255, 207, 53, 19);
+const Color stopGradient = Color.fromARGB(255, 255, 92, 92);
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer(startGradient, stopGradient)),
+    ),
+  );
 }
