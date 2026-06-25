337e0adfaa3dd1881e8ac38fbf0565c2b0851a95
vachan-maker <vachan@tutanota.com>
2026-06-25 15:02:51 +0530

    Added stateful widget and dice image changes

---
+++ lib/dice_roller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+var random = Random();
+var activeDiceImage = 'assets/images/dice-2.png';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/images/dice-4.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext build) {
+    return (Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(activeDiceImage, width: 200),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.all(20),
+            textStyle: const TextStyle(fontSize: 28, color: Colors.black),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    ));
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,35 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:rolldice/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.startColor, this.stopColor, {super.key});
   final Color startColor;
   final Color stopColor;
 
-  void rollDice() {}
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [startColor, stopColor]),
       ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-2.png', width: 200),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.all(20),
-                textStyle: const TextStyle(fontSize: 28, color: Colors.black),
-              ),
-              child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
-      ),
+      child: Center(child: DiceRoller()),
     );
   }
 }
