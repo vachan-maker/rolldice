4642e413c89d1a70a1f17133f69ead9c74d2acd8
vachan-maker <vachan@tutanota.com>
2026-06-25 14:25:30 +0530

    Added dice and button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:rolldice/text_container.dart';
-
-const Color startGradient = Color.fromARGB(255, 207, 53, 19);
-const Color stopGradient = Color.fromARGB(255, 255, 92, 92);
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.startColor, this.stopColor, {super.key});
   final Color startColor;
   final Color stopColor;
 
+  void rollDice() {}
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [startColor, stopColor]),
       ), //supposed to add a gradint here but I am not going to add it. I am jjust tired
-      child: Center(child: TextContainer('My Name is Vachan')),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 200),
+            TextButton(onPressed: rollDice, child: const Text('Roll Dice')),
+          ],
+        ),
+      ),
     );
   }
 }
