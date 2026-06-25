32022917f29f46ff7742d451055368a6c67d9f75
vachan-maker <vachan@tutanota.com>
2026-06-25 14:04:25 +0530

    set

--- lib/text_container.dart
+++ lib/text_container.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class TextContainer extends StatelessWidget {
   const TextContainer(this.text, {super.key});
-  String text;
+  final String text;
   @override
   Widget build(BuildContext context) {
     return (Text(text, style: TextStyle(color: Colors.white, fontSize: 28)));
